9668a9f193a5fafe3aa833c04e02a9642c803f0b
Pistos <github.pistos@purepistos.net>
2013-05-19 13:14:56 -0400

    Don't try to set tmux window name if session name is not set, or is empty.

--- git-branch.rb
+++ git-branch.rb
@@ -1,5 +1,8 @@
 tmux_rename_window_proc = Proc.new do |buffer|
-  `tmux rename-window #{$diakonos.session["name"]}`
+  session_name = $diakonos.session["name"]
+  if session_name && ! session_name.strip.empty?
+    `tmux rename-window #{session_name}`
+  end
 end
 
 $diakonos.register_proc( tmux_rename_window_proc, :after_startup )
