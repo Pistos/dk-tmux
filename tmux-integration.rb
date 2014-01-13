cdc7b65b7187ca3dc3b93b38ccf94ec8a647a1c5
Pistos <github.pistos@purepistos.net>
2014-01-13 12:44:41 -0500

    Update to be compatible with current Diakonos code (which has a Session class now).

--- tmux-integration.rb
+++ tmux-integration.rb
@@ -1,5 +1,5 @@
 tmux_rename_window_proc = Proc.new do |buffer|
-  session_name = $diakonos.session["name"]
+  session_name = $diakonos.session.name
   if session_name && ! session_name.strip.empty?
     `tmux rename-window #{session_name}`
   end
